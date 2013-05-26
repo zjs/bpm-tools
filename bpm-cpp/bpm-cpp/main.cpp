e8a19492a5d5830649bdccc5e7f7c1b06536b58f
jimjibone <jamesreuss@gmail.com>
2013-05-26 11:05:23 +0100

    created c++ testing project

---
+++ bpm-cpp/bpm-cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bpm-cpp
+//
+//  Created by James Reuss on 26/05/2013.
+//  Copyright (c) 2013 Darksky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
